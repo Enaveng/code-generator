62ddd15ca150b53b95435c9005c31db4ceb10a26
吴涛 <wutao@andwyf.com>
2024-03-12 09:14:51 +0800

    云平台后端项目初始化

---
+++ code-generator-web-backend/src/main/java/com/enaveng/generatorweb/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.enaveng.generatorweb.exception;
+
+import com.enaveng.generatorweb.common.BaseResponse;
+import com.enaveng.generatorweb.common.ResultUtils;
+import com.enaveng.generatorweb.common.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+  
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ code-generator-web-backend/src/main/java/com/enaveng/generatorweb/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.enaveng.generatorweb.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.enaveng.generatorweb.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+  
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
