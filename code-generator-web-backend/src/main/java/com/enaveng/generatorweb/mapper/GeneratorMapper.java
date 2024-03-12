017482800c08aa01adf93acba895e5f73baaf27b
吴涛 <wutao@andwyf.com>
2024-03-12 14:18:41 +0800

    提交未提交的代码

---
+++ code-generator-web-backend/src/main/java/com/enaveng/generatorweb/mapper/GeneratorMapper.java
@@ -0,0 +1,18 @@
+package com.enaveng.generatorweb.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.enaveng.generatorweb.model.entity.Generator;
+
+/**
+ * @author 86158
+ * @description 针对表【generator(代码生成器)】的数据库操作Mapper
+ * @createDate 2024-03-12 09:43:58
+ * @Entity com.enaveng.generatorweb.model.entity.Generator
+ */
+public interface GeneratorMapper extends BaseMapper<Generator> {
+
+}
+
+
+
+
